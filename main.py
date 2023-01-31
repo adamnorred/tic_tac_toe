a1365384afa58e752e7820f495044cb9d387c5c0
adamnorred <adamderron@gmail.com>
2023-01-31 23:22:25 +0100

    class implement test

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ FIELD_DICT = {'1': ' ', '2': ' ', '3': ' ', '4': ' ', '5': ' ', '6': ' ', '7': '
 WIN_ROWS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
 
 
-tutorial.tutorial()
+start_tutorial = tutorial.Tutorial()
+
 
 # for restarting fresh game
 while True:
--- tutorial.py
+++ tutorial.py
@@ -1,20 +1,31 @@
+from os import system
 
 """
 Guides user how to play every time app launches.
 """
+class Tutorial:
+    def __init__(self):
+        is_on = True
+        while is_on:
+            system('cls')
+            self.print_board()
+            is_on = self.condition(self.tutorial_question())
 
-# shows 'GUI' to the user
-def tutorial():
-    while True:
+    @staticmethod
+    def print_board():
         print('\n\n 1 | 2 | 3')
         print('-' * 11)
         print(' 4 | 5 | 6')
         print('-' * 11)
         print(' 7 | 8 | 9')
 
-        user_tutorial_question = input('\nEach field corresponds to number on your keyboard. '
-                                       'Press 1-9 to enter X or O.\nType "y" to continue: ')
-        if user_tutorial_question.lower() == 'y':
-            break
-        else:
-            continue
+    @staticmethod
+    def condition(answer):
+        if answer.lower() == 'y':
+            return False
+        return True
+
+    @staticmethod
+    def tutorial_question():
+        question = input('\nEach field corresponds to number on your keyboard. Press 1-9 to enter X or O.\nType "y" to continue: ')
+        return question
